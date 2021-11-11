5b0cf66d68cf6f8c133bb82d039367d4d392fe5d
Pavithra2304 <92969170+Pavithra2304@users.noreply.github.com>
2021-11-11 11:17:10 +0530

    Add files via upload

---
+++ JobsDetails/JobsDetails/Job.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobsDetails
+{
+    class Job
+    {
+        string role;
+        string location;
+        int exp;
+        double salary;
+
+        public Job(string role,string location,int exp,double salary)
+        {
+            this.Role = role;
+            this.Location = location;
+            this.Exp = exp;
+            this.Salary = salary;
+        }
+
+        public string Role { get => role; set => role = value; }
+        public string Location { get => location; set => location = value; }
+        public int Exp { get => exp; set => exp = value; }
+        public double Salary { get => salary; set => salary = value; }
+
+        public override string ToString()
+        {
+            return string.Format("{0,15} {1,15} {2,15} {3,15} ", Role, Location, Exp, Salary);
+        }
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ JobsDetails/JobsDetails/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobsDetails
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            int count_job;
+            Console.WriteLine("Please Enter the jobs do you want to Enter:");
+            count_job = Convert.ToInt32(Console.ReadLine());
+            List<Job> j1 = new List<Job>();
+            int press = 0;
+
+            for (int i = 0; i < count_job; i++)
+            {
+                Console.WriteLine("Enter the Job Details as follows:Role,Location,Experience,Salary");
+                string job = Console.ReadLine();
+                string[] job1 = job.Split(',');
+                Job job_Details = new Job(job1[0], job1[1], int.Parse(job1[2]), double.Parse(job1[3]));
+                j1.Add(job_Details);
+
+
+            }
+            Console.WriteLine("Press 1 for sorting based on Location:Press 2 for sorting based on Salary:");
+            press = Convert.ToInt32(Console.ReadLine());
+            JobFilter j = new JobFilter();
+            List<Job> sort = new List<Job>();
+            switch (press)
+            {
+                case 1:
+                    Console.WriteLine("Please Enter the Location");
+                    sort = j.findJob(j1, Console.ReadLine());
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the Salary:");
+                    sort = j.findJob(j1, Convert.ToDouble(Console.ReadLine()));
+                    break;
+
+            }
+            foreach (var item in sort)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
